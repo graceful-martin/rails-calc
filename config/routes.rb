d4570567fb1b05bbe66bd67fe78c62d4d3273843
in-genieur <20160658@gm.hannam.ac.kr>
2021-01-04 16:44:16 +0900

    계산기 완성

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 	get '/add' => 'calculator#add'
-  get '/result' => 'calculator#result'
+  post '/result' => 'calculator#result'
+  get '/result/:param1/:param2' => 'calculator#result'
 end
